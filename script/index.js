1a3d5a6a5cc94533cb6c11b3c40496dde698ab4e
Kevin <691208yz@gmail.com>
2020-04-19 15:23:45 -0400

    add company search

--- script/index.js
+++ script/index.js
@@ -1,29 +1,73 @@
 const apiKey = "QTDS1EF17V0Z9820";
-let url = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=IBM&apikey=${apiKey}`;
-let years = 5;
+let symbol = "IBM";
+let url = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=${apiKey}`;
+let monthTot, sylurl;
 
-$('.form-1').on('submit', (evt)=>{
-    years = $('.input-1').val();
-})
+// Choose year and month
+$(".dropdown-year").on("click", (evt) => {
+  $(".input-year").val(evt.target.text);
+});
 
-console.log('this is years', years)
+$(".dropdown-month").on("click", (evt) => {
+  $(".input-month").val(evt.target.text);
+});
 
-$('.dropdown-item').on('click',(evt)=>{
-    $('.input-1').val(evt.target.text);
-})
+// Choose company
+$("#basic-addon2").on("click", () => {
+  let table = `<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Symbol</th>
+      <th scope="col">Company</th>
+    </tr>
+  </thead>
+  <tbody>
+  </tbody>
+  </table>`;
+  $(".company-display").html(table);
+  symbol = $(".company-name").val();
+  sylurl = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${symbol}&apikey=${apiKey}`;
+  $.get(sylurl)
+    .done((res) => {
+      const companies = res["bestMatches"];
+      companies.forEach((company) => {
+        company["1. symbol"];
+        $("tbody").append(`<tr class='stock-sym'>
+      <th scope="row">${company["1. symbol"]}</th>
+      <td>${company["2. name"]}</td>
+    </tr>
+    `);
+      })
+      $("tbody tr").on("click", function (evt) {
+        symbol = evt.currentTarget.cells[0].innerText
+        $('#company-symbol-3').html(symbol);
+        url = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=${apiKey}`;
+        evt.stopPropagation();
+      });
+    })
+    .fail((err) => {
+      console.log("Error: ", err);
+    });
+});
 
 
-$("#stockbtn").on("click", () => {
+// Calculate appreciation
+$(".form-1").on("submit", () => {
+  monthTot = $(".input-year").val() * 12.0 + $(".input-month").val() * 1.0;
+
   $.get(url)
     .done((res) => {
       const sylbol = res["Meta Data"]["2. Symbol"];
       const stockMonthAndPrice = res["Monthly Time Series"];
       const months = Object.keys(stockMonthAndPrice);
-      const lastMonth = months[years * 12] || months[months.length - 1];
+      const lastMonth = months[monthTot] || months[months.length - 1];
       const currentMonth = months[0];
       const lastPrice = stockMonthAndPrice[lastMonth]["4. close"];
       const currentPrice = stockMonthAndPrice[currentMonth]["4. close"];
-      const appreciation = ((currentPrice - lastPrice)/lastPrice*100).toFixed(2);
+      const appreciation = (
+        ((currentPrice - lastPrice) / lastPrice) *
+        100
+      ).toFixed(2);
       $("#stockp").html(
         `The price of ${sylbol} is $${currentPrice} as of ${currentMonth}
         <br>The price of ${sylbol} was $${lastPrice} as of ${lastMonth}
