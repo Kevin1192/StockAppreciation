35d1a41960a7120e403beac1c54bd8d7dc78b800
Kevin <691208yz@gmail.com>
2020-04-19 20:59:22 -0400

    add try random stock

--- script/index.js
+++ script/index.js
@@ -37,10 +37,11 @@ $("#basic-addon2").on("click", () => {
       <td>${company["2. name"]}</td>
     </tr>
     `);
-      })
+      });
       $("tbody tr").on("click", function (evt) {
-        symbol = evt.currentTarget.cells[0].innerText
-        $('#company-symbol-3').html(symbol);
+        console.log($(this))
+        symbol = evt.currentTarget.cells[0].innerText;
+        $("#company-symbol-3").html(symbol);
         url = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=${apiKey}`;
         evt.stopPropagation();
       });
@@ -50,32 +51,66 @@ $("#basic-addon2").on("click", () => {
     });
 });
 
+// Try random company
+$(".try-random").on("click", () => {
+  symbol = "";
+  for (let i = 0; i < 3; i++) {
+    const letter = String.fromCharCode(97.0 + Math.round(Math.random() * 25.0));
+    symbol += letter;
+  }
+  sylurl = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${symbol}&apikey=${apiKey}`;
+  $.get(sylurl)
+    .done((res) => {
+      const companies = res["bestMatches"];
+      symbol = companies[0]['1. symbol']
+      console.log('s2', symbol)
+      url = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=${apiKey}`;
+      $.get(url).done((res) => {
+        console.log(res)
+        appreciate(res);
+      }).fail(err=> console.log(err));
+    })
+    .fail((err) => {
+      console.log("error", err);
+    });
+});
 
 // Calculate appreciation
-$(".form-1").on("submit", () => {
+$(".form-1").on("submit", (evt) => {
+  evt.preventDefault();
+  console.log("dddf");
   monthTot = $(".input-year").val() * 12.0 + $(".input-month").val() * 1.0;
-
   $.get(url)
     .done((res) => {
-      const sylbol = res["Meta Data"]["2. Symbol"];
-      const stockMonthAndPrice = res["Monthly Time Series"];
-      const months = Object.keys(stockMonthAndPrice);
-      const lastMonth = months[monthTot] || months[months.length - 1];
-      const currentMonth = months[0];
-      const lastPrice = stockMonthAndPrice[lastMonth]["4. close"];
-      const currentPrice = stockMonthAndPrice[currentMonth]["4. close"];
-      const appreciation = (
-        ((currentPrice - lastPrice) / lastPrice) *
-        100
-      ).toFixed(2);
-      $("#stockp").html(
-        `The price of ${sylbol} is $${currentPrice} as of ${currentMonth}
-        <br>The price of ${sylbol} was $${lastPrice} as of ${lastMonth}
-        <br><h2>The total appreciation is ${appreciation}%</h2>
-        `
-      );
+      appreciate(res);
     })
     .fail((err) => {
       console.log("Error: ", err);
     });
 });
+
+function appreciate(res) {
+  if (res.Note && res.Note.startsWith("Thank you for using Alpha Vantage!")){
+    alert('The api can only make 5 requests/min,  500/day. Please try again later')
+  }
+    const sylbol = res["Meta Data"]["2. Symbol"];
+  const stockMonthAndPrice = res["Monthly Time Series"];
+  const months = Object.keys(stockMonthAndPrice);
+  const lastMonth = months[monthTot] || months[months.length - 1];
+  const currentMonth = months[0];
+  const lastPrice = stockMonthAndPrice[lastMonth]["4. close"];
+  const currentPrice = stockMonthAndPrice[currentMonth]["4. close"];
+  const appreciation = (((currentPrice - lastPrice) / lastPrice) * 100).toFixed(2);
+  $("#company-symbol-3").html(sylbol);
+  $("#stockp").html(
+    `The price of ${sylbol} is <h2>$${currentPrice} </h2> as of ${currentMonth}
+        <hr>The price of ${sylbol} was <h2> $${lastPrice} </h2> as of ${lastMonth}
+        <hr><br><h2>Total appreciation: <span class='appre-color'>${appreciation}%</span></h2>
+        `
+  );
+  if (appreciation > 0) {
+    $(".appre-color").css({ color: "green" });
+  } else if (appreciation < 0) {
+    $(".appre-color").css({ color: "red" });
+  }
+}
